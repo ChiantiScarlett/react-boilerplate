83af9b38d59fc7eb26f60bdcf49fd28fc26d480f
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-09-12 14:21:20 +0900

    add default application component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import { viewRoutes } from "services/routes";
+import { RootContext, initialValue } from "services/context";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/**
+ * Wrapper for updating title
+ */
+const TitleUpdater = ({ title, children }) => {
+  document.title = title;
+  return <>{children}</>;
+};
+
+/**
+ * Core Application
+ */
+const App = () => {
+  const [context, setContext] = useState(initialValue);
+  return (
+    <RootContext.Provider value={[context, setContext]}>
+      <BrowserRouter>
+        <Switch>
+          {Object.keys(viewRoutes).map((key, idx) => (
+            <Route
+              exact
+              path={viewRoutes[key].path}
+              key={idx}
+              component={() => {
+                const Component = viewRoutes[key].component;
+                return (
+                  <TitleUpdater title={viewRoutes[key].title}>
+                    <Component />
+                  </TitleUpdater>
+                );
+              }}
+            />
+          ))}
+        </Switch>
+      </BrowserRouter>
+    </RootContext.Provider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
